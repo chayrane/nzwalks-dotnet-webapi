3a6f6be343b355c7a8430891b064454ba1f1f91a
Chaitanya Rane <68692629+chayrane@users.noreply.github.com>
2024-07-09 16:05:39 +0530

    created Regions Controller and get action methods

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers;
+
+// HTTP Endpoint: https://localhost:7258/api/regions
+[Route("api/[controller]")]
+[ApiController]
+public class RegionsController : ControllerBase
+{
+    public readonly NZWalksDbContext _dbContext;
+
+    public RegionsController(NZWalksDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    // Get All Regions
+    // GET: https://localhost:7258/api/regions
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var regions = _dbContext.Regions.ToList();
+
+        return Ok(regions);
+    }
+
+    // Get Region by Id.
+    // GET: https://localhost:7258/api/regions/:id
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult Get([FromRoute] Guid id)
+    {
+        var region = _dbContext.Regions.Find(id);
+
+        // var region = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+        if (region is null) return NotFound();
+
+        return Ok(region);
+    }
+
+}
