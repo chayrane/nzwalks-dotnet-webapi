f66230779bf34fec8449735572dd09016d09de7a
Chaitanya Rane <68692629+chayrane@users.noreply.github.com>
2024-07-09 14:29:20 +0530

    Create DbContext class and add DbSets

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data;
+
+public class NZWalksDbContext : DbContext
+{
+    public NZWalksDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    // DbSets - Property of DbContext class that represents
+    //          a collections of entities in the database.
+    public DbSet<Walk> Walks { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Difficulty> Difficulty { get; set; }
+}
